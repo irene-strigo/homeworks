e9c687a3b3ce2440cff35a6217667c4e4c180250
Irene Strigo <irene.strigo@gmail.com>
2022-08-26 14:48:52 +0200

    homework week13

---
+++ week13/index.js
@@ -0,0 +1,85 @@
+//alert('Laku noch')
+
+//let message ='Dobro jutro!';
+//let x = 5
+//let y = 3;
+
+//типы данных
+//числа
+//const CONST = 100;
+//n = 1.23;
+//alert (x * y);
+//alert ( n + 10);
+//alert ( n/0 );
+
+//строки
+let name1 = "Irina";
+let name2 = 'Ira';
+let name3 = `Ira!`
+let message_to = `Привет, ${name1}`;
+//alert (message_to);
+
+let message_to2 = 'Привет, '+ name3;//устаревший способ
+//alert (message_to2);
+//буллевые
+//let yes = true;
+let no = false;
+//специальные
+let e = null;
+
+//операторы
+//let a = 10;
+//let b = 2;
+let c = a ** b; //степень
+//alert (c);
+
+a += 5 //это a = a + 5
+let message3 = "Бля";
+message3 += "!";
+//alert (message3);
+
+let d = '2';
+let f = '3';
+//alert (Number (d) + Number(f));
+//или!
+//alert (+a + +b);
+
+//alert (String (5));//редко бывает нужно, всё и так выводится как строка
+
+//alert (Boolean (1));//это true
+//alert (Boolean (0));//это false
+//alert (Boolean ("0"));//а это тоже true, потому что здесь 0 строка, заполненная символом
+
+//функции:
+
+
+//let age = prompt ("сколько тебе лет?");
+//alert (`тебе ${age}`)
+
+//let yes = confirm ('продолжим разбирать JS?');
+//alert ('ты сказала'  + yes)
+
+//function showMessage()
+//{ alert ('привет лунатикам!')};
+//function sum (a,b)
+{return a + b;
+}
+//alert (sum (12,3));
+
+
+let name = "Ilya";
+//alert (`hello ${1}`);
+//alert (`hello ${"name"}`);
+//alert (`hello ${name}`)
+
+var x = 5;         
+var y = 2;        
+var z = x + y;
+//alert(z);
+
+//function showX(x)
+{ 
+	return x;
+}
+//alert(showX(28));
+
